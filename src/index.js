7a98c858247cc9f31e7322373650c1351aa8174b
Isabelle Pouget <isab.pouget@gmail.com>
2021-01-29 20:46:25 +0100

    Put countries app with clean npm webpack and bootstrap

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class ApiCall {
+  constructor() {
+    this.baseUrl = "";
+  }
+
+  setBaseUrl(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  getBaseUrl() {
+    return this.baseUrl;
+  }
+
+  get(endpoint) {
+    return fetch(this.baseUrl + endpoint).then(response => response.json());
+  }
+}
+
+const api = new ApiCall();
+api.setBaseUrl("https://restcountries.eu/rest/v2/");
+
+const button = document.querySelector("#go");
+const name = document.querySelector("#name");
+
+const infos = document.querySelector("#infos");
+const flag = document.querySelector("#flag");
+
+const message = document.querySelector("#message");
+
+
+button.addEventListener("click", event => {
+  event.preventDefault();
+  infos.innerHTML=""
+  flag.innerHTML=""
+  message.innerHTML=""
+  api
+    .get(`name/${name.value}`)
+    .then(data => {
+      const [countryData] = data
+      const{name,region,capital,currencies,languages} = countryData
+      const[currencyCountry] = currencies
+      const[languageCountry] = languages
+      infos.insertAdjacentHTML("beforeend",
+      `<ul class="list-unstyled bg-light p-3 rounded text-center">
+      <h2 class="pb-4 ">${name}</h2>
+        <li>Continent : ${region}</li>
+        <li>Capital : ${capital}</li>
+        <li>Currency : ${currencyCountry.name}</li>
+        <li>Language : ${languageCountry.name}</li>
+      </ul>`);
+      flag.insertAdjacentHTML("beforeend", `<img src="${data[0].flag}" width=75%/>`);
+    })
+    .catch((error) => {
+      message.insertAdjacentHTML("beforeend", '<div class="alert alert-primary text-center" role="alert">Try again! Write in English.</div>');
+    });
+});
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+ "mode": "none",
+ "entry": "./src/index.js",
+ "output": {
+   "path": __dirname + '/dist',
+    publicPath: "/APIcountries/",
+   "filename": "bundle.js"
+ },
+ devServer: {
+   contentBase: path.join(__dirname, 'dist')
+ },
+ "module": {
+   "rules": [
+     {
+       "test": /\.css$/,
+       "use": [
+         "style-loader",
+         "css-loader"
+       ]
+     },
+   ]
+ }
+};
